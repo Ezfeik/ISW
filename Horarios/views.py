3b1c3e169695ca6e97d46b9b72738933726f9f57
awerito <diegoignacio.munoz@alumnos.ulagos.cl>
2019-06-10 10:32:21 -0400

    Template para la página de inicio

--- Horarios/views.py
+++ Horarios/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Página de Inicio de Horarios")
+    return render(request, "Horarios/index.html")
