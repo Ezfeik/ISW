39330ef832d7e8f8831c3879e5fd1edca4f4df76
Ezfeik <spvo_2007@hotmail.com>
2019-06-11 14:48:26 -0400

    Agregado archivo profesor.html + vistas

--- Horarios/urls.py
+++ Horarios/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profesor/', views.profesor, name='profesor'),
 ]
--- Horarios/views.py
+++ Horarios/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "Horarios/index.html")
+
+def profesor(request):
+    return render(request, "Horarios/profesor.html")
