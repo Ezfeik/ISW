0d4ebe065ba5614b1095936f022c1b7eea87ef26
Ezfeik <spvo_2007@hotmail.com>
2019-06-25 09:02:01 -0400

    Clase Admin,Profesor,Bloque modificable/añadible en admin

--- Horarios/admin.py
+++ Horarios/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Horario, Bloque, Profesor
 
 # Register your models here.
+
+class HorarioAdmin(admin.ModelAdmin):
+    list_display = [('nombre')]
+
+class BloqueAdmin(admin.ModelAdmin):
+    list_display = ('nombre','h_inicio','h_fin')
+
+class ProfeAdmin(admin.ModelAdmin):
+    list_display = ('usuario','nombres','apellidos','dias_disp')
+
+
+admin.site.register(Profesor, ProfeAdmin)
+admin.site.register(Horario, HorarioAdmin)
+admin.site.register(Bloque, BloqueAdmin)
